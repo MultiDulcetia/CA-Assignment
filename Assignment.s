478d4c233ce785546cc55c29c5d436eeb5011aec
MultiDulcetia <155560738+MultiDulcetia@users.noreply.github.com>
2024-02-04 23:14:54 +0800

    Create Assignment.s

---
+++ Assignment.s
@@ -0,0 +1,14 @@
+       MOV     R1,#0x5000
+       MOV     R0, #0x6
+       STRB    R0,[R1],#1
+       MOV     R0,#0x5
+       STRB    R0,[R1],#1
+       MOV     R0,#0x1
+       STRB    R0,[R1],#1
+       MOV     R0,#0x6
+       STRB    R0,[R1],#1
+       MOV     R0,#0x3
+       STRB    R0,[R1],#1
+       MOV     R0,#0x5
+       STRB    R0,[R1]
+       MOV     R1,#0x5000
